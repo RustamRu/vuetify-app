bfaefa8c3cfb258a53bc3e0647a96748bec0c20a
RustamRu <kagapov.rustam@gmail.com>
2023-04-21 18:59:34 +0300

    modules, plugins

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ],
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "~@/scss/typography.scss";
+          @import "~@/scss/app-variables.scss";
+          @import "~@/scss/variables.scss";
+          @import "~@/scss/common.scss";
+        `,
+      }
+    }
+  }
 })
